c7ecf36f3d325c54185fadd62fd933dba86722a6
VogimKof <99262814+VogimKof@users.noreply.github.com>
2024-11-01 17:52:10 +0100

    Update Code.js

--- Kalkulator/Code.js
+++ Kalkulator/Code.js
@@ -1,14 +1,17 @@
-const expressionElement = document.getElementById('expression');
+
 
 function addToExpression(value) {
+    const expressionElement = document.getElementById('expression');
     expressionElement.textContent += value; // Append button value to paragraph
 }
 
 function Erase() {
+    const expressionElement = document.getElementById('expression');
     expressionElement.textContent = ''; // Clear the paragraph
 }
 
 function Evaluate() {
+    const expressionElement = document.getElementById('expression');
     try {
         const result = eval(expressionElement.textContent);
         expressionElement.textContent = result; // Show result in paragraph
@@ -18,5 +21,6 @@ function Evaluate() {
 }
 
 function Cofnij(){
+    const expressionElement = document.getElementById('expression');
     expressionElement.textContent = expressionElement.textContent.slice(0, -1);
 }
